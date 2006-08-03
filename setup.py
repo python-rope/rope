78605fadd7ca5bc43c7f82a103625bf0841fafee
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2006-08-03 12:05:01 +0330

    Renaming rope script from rope.py to rope

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import os
 from distutils.core import setup
+
+def make_scripts():
+    if not os.path.exists('scripts'):
+        os.mkdir('scripts')
+    rope_py_text = file('rope.py').read()
+    rope_script_file = file('scripts/rope', 'w')
+    rope_script_file.write(rope_py_text)
+    rope_script_file.close()
+
+make_scripts()
 
 setup(name='rope',
       version='0.2',
@@ -6,6 +17,6 @@ setup(name='rope',
       author='Ali Gholami Rudi',
       author_email='aligrudi@users.sourceforge.net',
       url='http://rope.sf.net/',
-      packages=['rope'],
-      scripts=['rope.py'])
+      packages=['rope', 'rope.ui'],
+      scripts=['scripts/rope'])
 
