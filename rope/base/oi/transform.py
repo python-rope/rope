c9750cfc51f0c4f99da6e504a5dc11449ff1d3af
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2008-02-28 10:37:48 +0330

    transform: fixed path_to_resource on dos

--- rope/base/oi/transform.py
+++ rope/base/oi/transform.py
@@ -210,7 +210,7 @@ class TextualToPyObject(object):
             root = self.project.address
             if not os.path.isabs(path):
                 return self.project.get_resource(path)
-            if path == root or path.startswith(root + '/'):
+            if path == root or path.startswith(root + os.sep):
                 # INFO: This is a project file; should not be absolute
                 return None
             import rope.base.project
@@ -279,7 +279,7 @@ class DOITextualToPyObject(TextualToPyObject):
     def path_to_resource(self, path):
         root = self.project.address
         if os.path.isabs(path) and (path == root or
-                                    path.startswith(root + '/')):
+                                    path.startswith(root + os.sep)):
             path = path[len(root):].replace(os.sep, '/')
             if path.startswith('/'):
                 path = path[1:]
