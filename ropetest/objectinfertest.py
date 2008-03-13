a1e6e281bec55ce579b108436ae206dfd295df0b
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2008-03-13 18:30:24 +0330

    objectinfertest: added an ignored test for property as decorator

--- ropetest/objectinfertest.py
+++ ropetest/objectinfertest.py
@@ -294,6 +294,17 @@ class ObjectInferTest(unittest.TestCase):
         e_var = mod['e'].get_object()
         self.assertEquals(my_error, e_var.get_type())
 
+    # TODO: properties as decorators
+    def xxx_test_using_property_as_decorators(self):
+        mod = self.pycore.get_string_module(
+            'class A(object):\n    pass\n'
+            'class B(object):\n    '
+            '@property\n    def f(self):\n        return A()\n'
+            'b = B()\nvar = b.f\n')
+        var = mod['var'].get_object()
+        a = mod['A'].get_object()
+        self.assertEquals(a, var.get_type())
+
 
 def suite():
     result = unittest.TestSuite()
