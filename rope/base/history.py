a7cbb6dbc0ecc3d7d406fe0355ef684dc172bc31
Ali Gholami Rudi <aligrudi@users.sourceforge.net>
2008-07-15 19:24:10 +0430

    history: renamed ChangeStack.changes to stack

--- rope/base/history.py
+++ rope/base/history.py
@@ -204,19 +204,19 @@ class ChangeStack(object):
     def __init__(self, project, description='merged changes'):
         self.project = project
         self.description = description
-        self.changes = []
+        self.stack = []
 
     def push_change(self, changes):
-        self.changes.append(changes)
+        self.stack.append(changes)
         self.project.do(changes)
 
     def pop_all(self):
-        for i in range(len(self.changes)):
+        for i in range(len(self.stack)):
             self.project.history.undo()
 
     def merged_changes(self):
         result = change.ChangeSet(self.description)
-        for changes in self.changes:
+        for changes in self.stack:
             for c in self._basic_changes(changes):
                 result.add_change(c)
         return result
