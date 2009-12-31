4cf06357e60916797922e3b068bbc06ab53ff67c
angri <unknown>
2009-12-31 14:43:17 +0300

    copyright: updated year numbers

--- rope/__init__.py
+++ rope/__init__.py
@@ -3,7 +3,8 @@
 INFO = __doc__
 VERSION = '0.9.2'
 COPYRIGHT = """\
-Copyright (C) 2006-2009 Ali Gholami Rudi
+Copyright (C) 2006-2010 Ali Gholami Rudi
+Copyright (C) 2009-2010 Anton Gritsay
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of GNU General Public License as published by the
