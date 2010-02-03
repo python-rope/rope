898ecd8ca63502cca36df63f4ff8de8b32daea34
angri <unknown>
2010-02-03 22:41:34 +0300

    rope: update version number to 0.9.3

--- rope/__init__.py
+++ rope/__init__.py
@@ -1,7 +1,7 @@
 """rope, a python refactoring library"""
 
 INFO = __doc__
-VERSION = '0.9.2'
+VERSION = '0.9.3'
 COPYRIGHT = """\
 Copyright (C) 2006-2010 Ali Gholami Rudi
 Copyright (C) 2009-2010 Anton Gritsay
