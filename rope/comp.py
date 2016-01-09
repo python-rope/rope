04ab99cbd352d4f40fc1b61cb8cf3638539985d8
emacsway <ivzak@yandex.ru>
2016-01-09 05:26:59 +0200

    minor clean useless variables

--- rope/comp.py
+++ rope/comp.py
@@ -7,13 +7,13 @@ PY34 = sys.version_info[0:2] >= (3, 4)
 try:
     str = unicode
 except NameError:  # PY3
-    py3 = True
+
     string_types = (str,)
     integer_types = (int,)
     import builtins
 
 else:  # PY2
-    py3 = False
+
     string_types = (basestring,)
     integer_types = (int, long)
     builtins = __import__('__builtin__')
