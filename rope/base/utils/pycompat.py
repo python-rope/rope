148474347ddd8ac0d1131f0eff9428d73a9f9014
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2016-02-17 03:54:50 +0100

    differences in python ast nodes
     * arg and Name and its usage in the language
     * difference in builtins in python 2 vs 3

---
+++ rope/base/utils/pycompat.py
@@ -0,0 +1,33 @@
+import sys
+import _ast
+# from rope.base import ast
+
+PY2 = sys.version_info[0] == 2
+PY27 = sys.version_info[0:2] >= (2, 7)
+PY3 = sys.version_info[0] == 3
+PY34 = sys.version_info[0:2] >= (3, 4)
+
+try:
+    str = unicode
+except NameError:  # PY3
+
+    str = str
+    string_types = (str,)
+    import builtins
+    ast_arg_type = _ast.arg
+
+    def get_ast_arg_arg(node):
+        if isinstance(node, string_types):  # TODO: G21: Understand the Algorithm (Where it's used?)
+            return node
+        return node.arg
+
+else:  # PY2
+
+    string_types = (basestring,)
+    builtins = __import__('__builtin__')
+    ast_arg_type = _ast.Name
+
+    def get_ast_arg_arg(node):
+        if isinstance(node, string_types):  # Python2 arguments.vararg, arguments.kwarg
+            return node
+        return node.id
