29a0c8f27b0d5d94ef0c2d9fbefb2428bc7ce5c2
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2016-02-17 03:27:42 +0100

    added more python versions to support in package

--- setup.py
+++ setup.py
@@ -42,6 +42,14 @@ classifiers = [
     'License :: OSI Approved :: GNU General Public License (GPL)',
     'Natural Language :: English',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Software Development']
 
 
