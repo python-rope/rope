c3ce2682a092161ca98b5a419472413751c0b78a
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-01-25 10:27:46 +0100

    remove distutils and automate package finding

--- setup.py
+++ setup.py
@@ -1,39 +1,8 @@
-import sys
 
-try:
-    from setuptools import Command, setup
-except ImportError:
-    from distutils.core import Command, setup
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+from setuptools import setup, find_packages
 
 import rope
 
-import ropetest
-import ropetest.contrib
-import ropetest.refactor
-
-
-class RunTests(Command):
-    """New setup.py command to run all tests for the package.
-    """
-    description = "run all tests for the package"
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        tests = unittest.TestSuite(ropetest.suite())
-        runner = unittest.TextTestRunner(verbosity=2)
-        results = runner.run(tests)
-        sys.exit(0 if results.wasSuccessful() else 1)
 
 
 classifiers = [
@@ -61,17 +30,16 @@ def get_long_description():
     end = lines.index('Getting Started')
     return '\n' + '\n'.join(lines[:end]) + '\n'
 
-setup(name='rope',
-      version=rope.VERSION,
-      description='a python refactoring library...',
-      long_description=get_long_description(),
-      author='Ali Gholami Rudi',
-      author_email='aligrudi@users.sourceforge.net',
-      url='https://github.com/python-rope/rope',
-      packages=['rope', 'rope.base', 'rope.base.oi', 'rope.refactor',
-                'rope.refactor.importutils', 'rope.contrib'],
-      license='GNU GPL',
-      classifiers=classifiers,
-      cmdclass={
-          'test': RunTests,
-      })
+setup(
+  name='rope',
+  version=rope.VERSION,
+  description='a python refactoring library...',
+  long_description=get_long_description(),
+  author='Ali Gholami Rudi',
+  author_email='aligrudi@users.sourceforge.net',
+  url='https://github.com/python-rope/rope',
+  packages=find_packages(exclude=('ropetest.*', 'sample_folder.*')),
+  license='GNU GPL',
+  classifiers=classifiers,
+  test_suite="ropetest.suite",
+)
