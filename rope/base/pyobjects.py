644fb2f9115bdbed77b4c9aa577b5bf0c1fbb39c
Matěj Cepl <mcepl@cepl.eu>
2020-02-16 22:56:58 +0100

    ast.Str and other constants are deprecated since 3.8.
    Fixes #279.

--- rope/base/pyobjects.py
+++ rope/base/pyobjects.py
@@ -220,8 +220,8 @@ class PyDefinedObject(object):
     def get_doc(self):
         if len(self.get_ast().body) > 0:
             expr = self.get_ast().body[0]
-            if isinstance(expr, ast.Expr) and \
-               isinstance(expr.value, ast.Str):
+            ast_Str = ast.Constant if hasattr(ast, "Constant") else ast.Str
+            if isinstance(expr, ast.Expr) and isinstance(expr.value, ast_Str):
                 docstring = expr.value.s
                 coding = self.get_module().coding
                 return _decode_data(docstring, coding)
