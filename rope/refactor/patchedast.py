f3ca93b77cf1a77f5a826d0d64bc5937fdcb0b4b
Lie Ryan <lie.1296@gmail.com>
2022-07-17 08:07:41 +1000

    Fix typo MatMul->MatMult

--- rope/refactor/patchedast.py
+++ rope/refactor/patchedast.py
@@ -277,7 +277,7 @@ class _PatchingASTWalker:
         "Div": "/",
         "Mod": "%",
         "Pow": "**",
-        "MatMul": "@",
+        "MatMult": "@",
         "LShift": "<<",
         "RShift": ">>",
         "BitOr": "|",
