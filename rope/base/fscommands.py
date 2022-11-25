3df320656cab547289a423bedc8d25f8bd10176a
Lie Ryan <lie.1296@gmail.com>
2022-11-25 18:15:14 +1100

    Annotate return value of unicode_to_file_data()

--- rope/base/fscommands.py
+++ rope/base/fscommands.py
@@ -12,11 +12,14 @@ import shutil
 import subprocess
 
 import rope.base.utils.pycompat as pycompat
+import typing
 
 try:
     unicode
 except NameError:
     unicode = str
+
+FileContent = typing.NewType("FileContent", bytes)
 
 
 def create_fscommands(root):
@@ -214,19 +217,19 @@ def _execute(args, cwd=None):
     return process.returncode
 
 
-def unicode_to_file_data(contents, encoding=None, newlines=None):
-    if not isinstance(contents, unicode):
+def unicode_to_file_data(contents: str, encoding=None, newlines=None) -> FileContent:
+    if not isinstance(contents, str):
         return contents
     if newlines and newlines != "\n":
         contents = contents.replace("\n", newlines)
     if encoding is None:
         encoding = read_str_coding(contents)
     if encoding is not None:
-        return contents.encode(encoding)
+        return FileContent(contents.encode(encoding))
     try:
-        return contents.encode()
+        return FileContent(contents.encode())
     except UnicodeEncodeError:
-        return contents.encode("utf-8")
+        return FileContent(contents.encode("utf-8"))
 
 
 def file_data_to_unicode(data, encoding=None):
