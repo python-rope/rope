02289a31786c62adbbfc4a76a9ee50b9e53b77f0
Edward K. Ream <edreamleo@gmail.com>
2022-11-27 05:10:18 -0600

    Explain an assert

--- rope/contrib/autoimport/__init__.py
+++ rope/contrib/autoimport/__init__.py
@@ -1,7 +1,7 @@
 """AutoImport module for rope."""
 from .pickle import AutoImport as _PickleAutoImport
 from .sqlite import AutoImport as _SqliteAutoImport
-assert _SqliteAutoImport
+assert _SqliteAutoImport  # Workaround for an apparent pyflakes bug.
 
 AutoImport = _PickleAutoImport
 
