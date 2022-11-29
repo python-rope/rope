3eba333df5a4e2596ed2ebaf7f51dd296907aac1
Lie Ryan <lie.1296@gmail.com>
2022-11-29 13:52:20 +1100

    Make ImportedModule's invariant explicit

--- rope/base/pynames.py
+++ rope/base/pynames.py
@@ -1,5 +1,14 @@
+from __future__ import annotations
+
+import typing
+
 import rope.base.pyobjects
 from rope.base import exceptions, utils
+
+
+if typing.TYPE_CHECKING:
+    from typing import Union
+    from rope.base import pyobjectsdef
 
 
 class PyName:
@@ -96,7 +105,18 @@ class ParameterName(PyName):
 
 
 class ImportedModule(PyName):
-    def __init__(self, importing_module, module_name=None, level=0, resource=None):
+    def __init__(
+        self,
+        importing_module: Union[
+            pyobjectsdef.PyModule,
+            pyobjectsdef.PyPackage,
+        ],
+        module_name=None,
+        level=0,
+        resource=None,
+    ):
+        assert module_name is not None or resource is not None, \
+            "At least one of module_name or resource must be set"
         self.importing_module = importing_module
         self.module_name = module_name
         self.level = level
@@ -115,7 +135,6 @@ class ImportedModule(PyName):
             if self.resource is not None:
                 self.pymodule.set(pycore.project.get_pymodule(self.resource))
             else:
-                assert isinstance(self.module_name, str)
                 try:
                     if self.level == 0:
                         pymodule = pycore.project.get_module(
