e565a5abc7869bdce00430c51335a7f9c1c3d5ec
Edward K. Ream <edreamleo@gmail.com>
2022-12-09 13:39:23 -0600

    Remove 'parse' from rope.base.ast. Not sure how it got there

--- rope/base/ast.py
+++ rope/base/ast.py
@@ -4,24 +4,6 @@ from ast import *
 from rope.base import fscommands
 
 
-def parse(source, filename="<string>"):
-    # NOTE: the raw string should be given to `compile` function
-    if isinstance(source, str):
-        source = fscommands.unicode_to_file_data(source)
-    if b"\r" in source:
-        source = source.replace(b"\r\n", b"\n").replace(b"\r", b"\n")
-    if not source.endswith(b"\n"):
-        source += b"\n"
-    try:
-        return ast.parse(source, filename="<unknown>")
-    except (TypeError, ValueError) as e:
-        error = SyntaxError()
-        error.lineno = 1
-        error.filename = filename
-        error.msg = str(e)
-        raise error
-
-
 def call_for_nodes(node, callback, recursive=False):
     """If callback returns `True` the child nodes are skipped"""
     result = callback(node)
