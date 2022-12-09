d643366597ecd547ca50a3f7e33f3d9608aa74e4
Edward K. Ream <edreamleo@gmail.com>
2022-12-09 09:57:05 -0600

    Add comments about *difficult* imports in pynames.py

--- rope/base/pynames.py
+++ rope/base/pynames.py
@@ -1,3 +1,4 @@
+# These imports are tricky. It's easy to cause circular imports.
 from __future__ import annotations
 
 import typing
@@ -7,6 +8,7 @@ from rope.base import exceptions, utils
 
 
 if typing.TYPE_CHECKING:
+    # pyobjectsdef appears only in annotations.
     from typing import Union
     from rope.base import pyobjectsdef
 
