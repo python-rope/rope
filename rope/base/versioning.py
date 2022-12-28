1d7bb7f8f1e5489134f8f21e11c8b16c1d1db4f6
Lie Ryan <lie.1296@gmail.com>
2022-12-28 22:09:11 +1100

    `calculate_version_hash()` now takes `project`

--- rope/base/versioning.py
+++ rope/base/versioning.py
@@ -1,8 +1,9 @@
 import hashlib
 
 import rope
+import rope.base.project
 
 
-def calculate_version_hash():
+def calculate_version_hash(project: rope.base.project.Project) -> str:
     hasher = hashlib.sha1(f"{rope.VERSION}".encode("ascii"))
     return hasher.hexdigest()
--- ropetest/versioningtest.py
+++ ropetest/versioningtest.py
@@ -3,17 +3,19 @@ from unittest.mock import patch
 from rope.base import versioning
 
 
-def test_calculate_version_hash():
-    version_hash = versioning.calculate_version_hash()
+def test_calculate_version_hash(project):
+    version_hash = versioning.calculate_version_hash(project)
     assert isinstance(version_hash, str)
 
-def test_version_hash_is_constant():
-    version_hash_1 = versioning.calculate_version_hash()
-    version_hash_2 = versioning.calculate_version_hash()
+
+def test_version_hash_is_constant(project):
+    version_hash_1 = versioning.calculate_version_hash(project)
+    version_hash_2 = versioning.calculate_version_hash(project)
     assert version_hash_1 == version_hash_2
 
-def test_version_hash_varies_on_rope_version():
-    actual_version_hash = versioning.calculate_version_hash()
+
+def test_version_hash_varies_on_rope_version(project):
+    actual_version_hash = versioning.calculate_version_hash(project)
     with patch("rope.VERSION", "1.0.0"):
-        patched_version_hash = versioning.calculate_version_hash()
+        patched_version_hash = versioning.calculate_version_hash(project)
     assert actual_version_hash != patched_version_hash
