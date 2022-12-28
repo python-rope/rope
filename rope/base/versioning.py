a6b5a746a55c6fc2eb8905092a6a281f47e7e5df
Lie Ryan <lie.1296@gmail.com>
2022-12-28 22:22:15 +1100

    Make version_hash vary on user preferences

--- rope/base/versioning.py
+++ rope/base/versioning.py
@@ -1,9 +1,23 @@
 import hashlib
+import json
 
 import rope
 import rope.base.project
 
 
 def calculate_version_hash(project: rope.base.project.Project) -> str:
-    hasher = hashlib.sha1(f"{rope.VERSION}".encode("ascii"))
+    hasher = hashlib.sha1()
+
+    version_data = f"{rope.VERSION}"
+    hasher.update(version_data.encode("ascii"))
+
+    serialized_prefs_data = _prefs_version_hash_data(project)
+    hasher.update(serialized_prefs_data.encode("ascii"))
+
     return hasher.hexdigest()
+
+
+def _prefs_version_hash_data(project):
+    prefs_data = dict(vars(project.prefs))
+    del prefs_data["callbacks"]
+    return json.dumps(prefs_data, sort_keys=True, indent=2)
--- ropetest/versioningtest.py
+++ ropetest/versioningtest.py
@@ -19,3 +19,11 @@ def test_version_hash_varies_on_rope_version(project):
     with patch("rope.VERSION", "1.0.0"):
         patched_version_hash = versioning.calculate_version_hash(project)
     assert actual_version_hash != patched_version_hash
+
+
+def test_version_hash_varies_on_user_preferences(project):
+    actual_version_hash = versioning.calculate_version_hash(project)
+    assert project.prefs.get("automatic_soa") is False
+    project.prefs.set("automatic_soa", True)
+    patched_version_hash = versioning.calculate_version_hash(project)
+    assert actual_version_hash != patched_version_hash
