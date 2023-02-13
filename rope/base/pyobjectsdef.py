6ca16a7a8e5a3b8721592a68512793eb181c79fb
Edward K. Ream <edreamleo@gmail.com>
2023-02-13 04:18:37 -0600

    One more super().__init__

--- rope/base/pyobjectsdef.py
+++ rope/base/pyobjectsdef.py
@@ -114,9 +114,7 @@ class PyFunction(pyobjects.PyFunction):
 class PyComprehension(pyobjects.PyComprehension):
     def __init__(self, pycore, ast_node, parent):
         self.visitor_class = _ComprehensionVisitor
-        rope.base.pyobjects.PyDefinedObject.__init__(
-            self, pycore, ast_node, parent, type_="Comp"
-        )
+        super().__init__(pycore, ast_node, parent, type_="Comp")
 
     def _create_scope(self):
         return rope.base.pyscopes.ComprehensionScope(
