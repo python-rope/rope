36b19ed9348951dc7a81f09114314877c860dcb6
Someone Serge <sergei.kozlukov@aalto.fi>
2023-11-09 00:50:29 +0000

    test_moving_modules_lazy_imports*: a test variant that still fails

--- ropetest/refactor/movetest.py
+++ ropetest/refactor/movetest.py
@@ -613,6 +613,26 @@ class MoveRefactoringTest(unittest.TestCase):
                 import pkg2.pkg3.pkg4.mod4""")
         self.assertEqual(expected, self.mod1.read())
 
+    # The previous test has only one occurrence of "lazy", which is inside an
+    # import statement. This test has an extra reference
+    def test_moving_modules_lazy_import_used(self):
+        pkg2 = testutils.create_package(self.project, "pkg2")
+        pkg3 = testutils.create_package(self.project, "pkg3", pkg2)
+        pkg4 = testutils.create_package(self.project, "pkg4", pkg3)
+        code = dedent("""\
+            def import_later():
+                import pkg.mod4
+                pkg.mod4""")
+        self.mod1.write(code)
+        self._move(self.mod4, None, pkg4)
+        self.assertTrue(self.project.find_module("pkg2.pkg3.pkg4.mod4") is not None)
+        expected = dedent("""\
+            def import_later():
+                import pkg2.pkg3.pkg4.mod4
+                pkg2.pkg3.pkg4.mod4""")
+        self.assertEqual(expected, self.mod1.read())
+
+
     def test_moving_package_with_from_and_normal_imports(self):
         pkg2 = testutils.create_package(self.project, "pkg2")
         code = dedent("""\
