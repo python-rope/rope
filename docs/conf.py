3680798135ee2ed6c046b3217495c4f8d9c9de4f
Lie Ryan <lie.1296@gmail.com>
2024-04-05 00:52:02 +1100

    Partially fix white-space wrapping in tables
    This still overflows, but it's much more readable than it was.

--- docs/conf.py
+++ docs/conf.py
@@ -62,6 +62,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["rope.css"]
 
 # Make sure the target is unique
 autosectionlabel_prefix_document = True
